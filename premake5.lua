daedd7a36e3f9475817a3451da9c94587cb3308c
Josh <josharms00@gmail.com>
2022-04-20 18:54:40 -0400

    Project setup.

---
+++ premake5.lua
@@ -0,0 +1,54 @@
+workspace "gl_sandbox"
+	startproject "gl_sandbox"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "gl_sandbox"
+	location "gl_sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++20"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/src",
+	}
+
+	filter "system:windows"
+		
+		systemversion "latest"
+
+		defines { "PLATFORM_WINDOWS" }
+
+
+	filter "system:linux"
+		systemversion "latest"
+
+		defines { "PLATFORM_LINUX" }
+    
+    filter "configurations:Debug"
+		defines "DEBUG"
+		runtime "Debug"
+		symbols "On"
+    
+    filter "configurations:Release"
+		defines "RELEASE"
+		runtime "Release"
+		optimize "On"
+
