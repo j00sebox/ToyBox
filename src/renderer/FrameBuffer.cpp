821f39d552030129ac386b98808f967efb38a075
Josh <josharms00@gmail.com>
2022-05-23 11:53:33 -0400

    Wrapped all the opengl call inside GL_CALL macro for error checking.

--- src/renderer/FrameBuffer.cpp
+++ src/renderer/FrameBuffer.cpp
@@ -8,7 +8,7 @@
 FrameBuffer::FrameBuffer(int width, int height)
     : m_width(width), m_height(height) 
 {
-    glGenFramebuffers(1, &m_id);
+    GL_CALL(glGenFramebuffers(1, &m_id));
 }
 
 FrameBuffer::FrameBuffer(FrameBuffer&& fbo)
@@ -19,7 +19,7 @@ FrameBuffer::FrameBuffer(FrameBuffer&& fbo)
 
 FrameBuffer::~FrameBuffer()
 {
-    glDeleteFramebuffers(1, &m_id);
+    GL_CALL(glDeleteFramebuffers(1, &m_id));
 }
 
 void FrameBuffer::attach_texture(AttachmentType attachment)
@@ -28,34 +28,34 @@ void FrameBuffer::attach_texture(AttachmentType attachment)
     {
         case AttachmentType::Colour:
         {
-            glGenTextures(1, &m_colour_attachment);
-            glBindTexture(GL_TEXTURE_2D, m_colour_attachment);
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, m_width, m_height, 0, GL_RGB, GL_UNSIGNED_INT, NULL);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-            glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_colour_attachment, 0);
+            GL_CALL(glGenTextures(1, &m_colour_attachment));
+            GL_CALL(glBindTexture(GL_TEXTURE_2D, m_colour_attachment));
+            GL_CALL(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, m_width, m_height, 0, GL_RGB, GL_UNSIGNED_INT, NULL));
+            GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
+            GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
+            GL_CALL(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_colour_attachment, 0));
             break;   
         }
 
         case AttachmentType::Depth:
         {
-            glGenTextures(1, &m_depth_attachment);
-            glBindTexture(GL_TEXTURE_2D, m_depth_attachment);
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, m_width, m_height, 0, GL_DEPTH_COMPONENT, GL_UNSIGNED_INT, NULL);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-            glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, m_depth_attachment, 0);
+            GL_CALL(glGenTextures(1, &m_depth_attachment));
+            GL_CALL(glBindTexture(GL_TEXTURE_2D, m_depth_attachment));
+            GL_CALL(glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, m_width, m_height, 0, GL_DEPTH_COMPONENT, GL_UNSIGNED_INT, NULL));
+            GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
+            GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
+            GL_CALL(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, m_depth_attachment, 0));
             break;   
         }
 
         case AttachmentType::Stencil:
         {
-            glGenTextures(1, &m_stencil_attachment);
-            glBindTexture(GL_TEXTURE_2D, m_stencil_attachment);
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_STENCIL_INDEX, m_width, m_height, 0, GL_STENCIL_INDEX, GL_UNSIGNED_INT, NULL);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-            glFramebufferTexture2D(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_TEXTURE_2D, m_stencil_attachment, 0);
+            GL_CALL(glGenTextures(1, &m_stencil_attachment));
+            GL_CALL(glBindTexture(GL_TEXTURE_2D, m_stencil_attachment));
+            GL_CALL(glTexImage2D(GL_TEXTURE_2D, 0, GL_STENCIL_INDEX, m_width, m_height, 0, GL_STENCIL_INDEX, GL_UNSIGNED_INT, NULL));
+            GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
+            GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
+            GL_CALL(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_TEXTURE_2D, m_stencil_attachment, 0));
             break;   
         }
     
@@ -70,28 +70,28 @@ void FrameBuffer::attach_renderbuffer(AttachmentType attachment)
     {
         case AttachmentType::Colour:
         {
-            glGenRenderbuffers(1, &m_colour_attachment);
-            glBindRenderbuffer(GL_RENDERBUFFER, m_colour_attachment);
-            glRenderbufferStorage(GL_RENDERBUFFER, GL_RGB, m_width, m_height);
-            glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, m_colour_attachment);
+            GL_CALL(glGenRenderbuffers(1, &m_colour_attachment));
+            GL_CALL(glBindRenderbuffer(GL_RENDERBUFFER, m_colour_attachment));
+            GL_CALL(glRenderbufferStorage(GL_RENDERBUFFER, GL_RGB, m_width, m_height));
+            GL_CALL(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, m_colour_attachment));
             break;   
         }
 
         case AttachmentType::Depth:
         {
-            glGenRenderbuffers(1, &m_depth_attachment);
-            glBindRenderbuffer(GL_RENDERBUFFER, m_depth_attachment);
-            glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, m_width, m_height);
-            glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, m_depth_attachment);
+            GL_CALL(glGenRenderbuffers(1, &m_depth_attachment));
+            GL_CALL(glBindRenderbuffer(GL_RENDERBUFFER, m_depth_attachment));
+            GL_CALL(glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, m_width, m_height));
+            GL_CALL(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, m_depth_attachment));
             break;   
         }
 
         case AttachmentType::Stencil:
         {
-            glGenRenderbuffers(1, &m_stencil_attachment);
-            glBindRenderbuffer(GL_RENDERBUFFER, m_stencil_attachment);
-            glRenderbufferStorage(GL_RENDERBUFFER, GL_STENCIL_INDEX, m_width, m_height);
-            glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, m_stencil_attachment);
+            GL_CALL(glGenRenderbuffers(1, &m_stencil_attachment));
+            GL_CALL(glBindRenderbuffer(GL_RENDERBUFFER, m_stencil_attachment));
+            GL_CALL(glRenderbufferStorage(GL_RENDERBUFFER, GL_STENCIL_INDEX, m_width, m_height));
+            GL_CALL(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, m_stencil_attachment));
             break;   
         }
     
@@ -102,13 +102,13 @@ void FrameBuffer::attach_renderbuffer(AttachmentType attachment)
 
 void FrameBuffer::bind() const
 {
-    glBindFramebuffer(GL_FRAMEBUFFER, m_id);
+    GL_CALL(glBindFramebuffer(GL_FRAMEBUFFER, m_id));
 }
 
 void FrameBuffer::unbind() const
 {
     // go back to using default frame buffer
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    GL_CALL(glBindFramebuffer(GL_FRAMEBUFFER, 0));
 }
 
 // fbo needs to be bound first
