c3a9bb7424cfab7a17799f37ed024335f9f55b4b
Josh <josharms00@gmail.com>
2022-06-04 16:44:39 -0400

    Fixed incorrect behaviour where render buffers were filled after the viewport and all the UI was displayed so it was displaying info from the previous frame. It also prevented aa from not working properly but it does now.

--- src/Application.cpp
+++ src/Application.cpp
@@ -28,14 +28,14 @@ void Application::start()
 
 		m_window.begin_frame();
 
+		float delta_time = m_window.get_delta_time();
+		m_current_scene->update(delta_time);
+
 		ImGui::ShowDemoWindow();
 		display_dockspace();
 		m_window.display_render_context();
 		display_menu();
 		display_fps();
-
-		float delta_time = m_window.get_delta_time();
-		m_current_scene->update(delta_time);
 
 		m_window.end_frame();
 	}
