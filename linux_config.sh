9d7e8bad4f6bf150c73541a3038c8fd5b9d61926
Josh <josharms00@gmail.com>
2023-04-18 13:29:33 -0400

    Created a python script to do the cmake configuration instead of having 2 different scripts for windows and linux.

--- linux_config.sh
+++
@@ -1,3 +0,0 @@
-mkdir build
-cp imgui.ini build
-cmake -DGLFW_BUILD_DOCS=OFF -S . -B build
--- windows_config.bat
+++
@@ -1,5 +0,0 @@
-call mkdir build
-call copy imgui.ini build
-call cmake -DGLFW_BUILD_DOCS=OFF -S . -B build
-
-PAUSE
